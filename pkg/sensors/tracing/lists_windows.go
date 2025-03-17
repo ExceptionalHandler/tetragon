3cc191921c049c024f7465306132fe1d91990eae
Anadi Sharma <aanadi@cisco.com>
2025-03-17 17:48:58 -0400

    Fix merge conflict

--- pkg/sensors/tracing/lists_windows.go
+++ pkg/sensors/tracing/lists_windows.go
@@ -4,9 +4,13 @@
 package tracing
 
 import (
-	"fmt"
+	"errors"
 
 	"github.com/cilium/tetragon/pkg/k8s/apis/cilium.io/v1alpha1"
+)
+
+var (
+	notSupportedWinErr = errors.New("not supported on windows")
 )
 
 // isList checks if a value specifies a list, and if so it returns it (or nil if list does not exist)
@@ -15,7 +19,7 @@ func isList(val string, lists []v1alpha1.ListSpec) (bool, *v1alpha1.ListSpec) {
 }
 
 func preValidateLists(lists []v1alpha1.ListSpec) (err error) {
-	return fmt.Errorf("not supported on windows")
+	return notSupportedWinErr
 }
 
 type listReader struct {
@@ -23,9 +27,9 @@ type listReader struct {
 }
 
 func (lr *listReader) Read(name string, ty uint32) ([]uint32, error) {
-	return []uint32{}, fmt.Errorf("Not supported on Windows")
+	return []uint32{}, notSupportedWinErr
 }
 
 func getSyscallListSymbols(list *v1alpha1.ListSpec) ([]string, error) {
-	return nil, fmt.Errorf("not supported on Windows")
+	return nil, notSupportedWinErr
 }
