8916a0b5731b8d157d50e08ce01e4c7d1882659b
Anadi Sharma <aanadi@cisco.com>
2025-03-17 18:17:44 -0400

    PolicyHandler stub

--- pkg/sensors/tracing/policyhandler_windows.go
+++ pkg/sensors/tracing/policyhandler_windows.go
@@ -5,7 +5,6 @@ package tracing
 
 import (
 	"errors"
-	"fmt"
 
 	"github.com/cilium/tetragon/pkg/policyfilter"
 	"github.com/cilium/tetragon/pkg/sensors"
@@ -16,28 +15,5 @@ func (h policyHandler) PolicyHandler(
 	policy tracingpolicy.TracingPolicy,
 	policyID policyfilter.PolicyID,
 ) (sensors.SensorIface, error) {
-
-	spec := policy.TpSpec()
-	sections := 0
-	if len(spec.KProbes) > 0 {
-		sections++
-	}
-	if len(spec.Tracepoints) > 0 {
-		sections++
-	}
-	if len(spec.LsmHooks) > 0 {
-		sections++
-	}
-	if len(spec.UProbes) > 0 {
-		sections++
-	}
-	if sections > 1 {
-		return nil, errors.New("tracing policies with multiple sections of kprobes, tracepoints, lsm hooks, or uprobes are currently not supported")
-	}
-
-	if len(spec.KProbes) > 0 {
-		return nil, fmt.Errorf("not supported on windows")
-
-	}
-	return nil, nil
+	return nil, errors.New("not supported on windows")
 }
