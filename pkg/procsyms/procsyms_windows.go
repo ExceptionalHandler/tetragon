efeb2891728b0d302c70a7bb8a620e1a038d3021
Anadi Sharma <aanadi@cisco.com>
2025-03-20 16:37:43 -0400

    procSysms package

--- pkg/procsyms/procsyms_windows.go
+++ pkg/procsyms/procsyms_windows.go
@@ -4,34 +4,11 @@
 package procsyms
 
 import (
-	"fmt"
-	"sync"
-
-	lru "github.com/hashicorp/golang-lru/v2"
+	"errors"
 )
-
-var (
-	cache *lru.Cache[struct {
-		module string
-		offset uint64
-	}, string]
-	setCache sync.Once
-)
-
-// FnSym is a function location (function name, module path + offset)
-type FnSym struct {
-	Name   string
-	Module string
-	Offset uint64
-}
-
-// ToString returns a string representation of FnSym
-func (fsym *FnSym) ToString() string {
-	return fmt.Sprintf("%s (%s+0x%x)", fsym.Name, fsym.Module, fsym.Offset)
-}
 
 // GetFnSymbol -- returns the FnSym for a given address and PID
 func GetFnSymbol(pid int, addr uint64) (*FnSym, error) {
-	return nil, fmt.Errorf("not implemented on windows (yet) ")
+	return nil, errors.New("not implemented on windows ")
 
 }
