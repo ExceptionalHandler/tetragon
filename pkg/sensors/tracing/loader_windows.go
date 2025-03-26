a9e7cf3b029ab09992806da4fc083ee522784889
Anadi Sharma <aanadi@cisco.com>
2025-03-26 14:05:55 -0400

    Remove old file

--- pkg/sensors/tracing/loader_windows.go
+++ pkg/sensors/tracing/loader_windows.go
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 package tracing
 
 import "errors"
@@ -7,13 +6,3 @@ func createLoaderEvents() error {
 
 	return errors.New("not supported on windows")
 }
-=======
-package tracing
-
-import "errors"
-
-func createLoaderEvents() error {
-
-	return errors.New("not supported on windows")
-}
->>>>>>> b8a6b5545 (Port the sensors/tracing package on Windows.)
