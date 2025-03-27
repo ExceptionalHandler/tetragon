340ae1947736d851f6fb13065b5a33d956363daa
Anadi Sharma <aanadi@cisco.com>
2025-03-26 21:10:28 -0400

    detect_windows

--- pkg/bpf/detect_windows.go
+++ pkg/bpf/detect_windows.go
@@ -33,6 +33,10 @@ func HasLinkPin() bool {
 	return false
 }
 
+func HasKprobeMulti() bool {
+	return false
+}
+
 func LogFeatures() string {
 	return fmt.Sprintf("override_return: %t, buildid: %t, fmodret: %t, fmodret_syscall: %t, signal: %t, large: %t, link_pin: %t",
 		HasOverrideHelper(), HasBuildId(), HasModifyReturn(), HasModifyReturnSyscall(), HasSignalHelper(), HasProgramLargeSize(),
